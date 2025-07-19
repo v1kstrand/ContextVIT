887c70108065c930a158d6c8c569bb3e6eed0f91
v1kstrand <david.vikstrand@example.com>
2025-07-20 00:02:04 +0200

    smal fix2

--- contextvit/config.py
+++ contextvit/config.py
@@ -106,7 +106,7 @@ def get_args(dict_args=None, check_args=False):
     # Running
     parser.add_argument("--epochs", type=int, default=2000)
     parser.add_argument("--batch_size", type=int, default=512)
-    parser.add_argument("--data_dir", type=str, default="")
+    parser.add_argument("--data_dir", type=str, default="/notebooks/data/imagenet_1k_resized_256")
     # Exp
     parser.add_argument("--skip_log_first_n", type=int, default=50)
     parser.add_argument("--freq", type=dict, default={})
